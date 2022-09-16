3a205ab16a10001e5a4f4d85042bb09eefa81369
Heber Cordova Jimenez <u201924464@upc.edu.pe>
2022-09-16 16:58:47 -0500

    Agregar archivos de proyecto.

---
+++ CalculatorService/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorService
+{
+    public class Calculator
+    {
+        public int sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int rest(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int factorial(int num)
+        {
+            if (num == 0)
+                return 1;
+            return num * factorial(num - 1);
+        }
+    }
+}
+
---
+++ CalculatorServiceTest/CalculatorTest.cs
@@ -0,0 +1,97 @@
+﻿using CalculatorService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CalculatorServiceTest
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void sumTest()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            int result = calculator.sum(3, 2);
+            int expected = 5;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void sumNegativeTest()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            int result = calculator.sum(-2, -3);
+            int expected = -5;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void restTest()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            int result = calculator.rest(5, 2);
+            int expected = 3;
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void restNegativeTest()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            int result = calculator.rest(-5, -2);
+            int expected = -3;
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void factorialZeroTest()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            int result = calculator.factorial(0);
+            int expected = 1;
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void factorialFiveTest()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            int result = calculator.factorial(5);
+            int expected = 120;
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
